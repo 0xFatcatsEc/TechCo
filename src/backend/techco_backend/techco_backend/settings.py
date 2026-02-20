3bdc7a6652cd780d9e43f09e7042fe41052f484b
FatCat <frutotimo@gmail.com>
2026-02-20 14:33:26 +0800

    fixing responsive

--- src/backend/techco_backend/techco_backend/settings.py
+++ src/backend/techco_backend/techco_backend/settings.py
@@ -101,8 +101,8 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv('DB_NAME', 'techco_db'),
-            'USER': os.getenv('DB_USER', ''),
-            'PASSWORD': os.getenv('DB_PASSWORD', ''),
+            'USER': os.getenv('DB_USER', 'postgres'),
+            'PASSWORD': os.getenv('DB_PASSWORD', 'admin@123'),
             'HOST': os.getenv('DB_HOST', 'localhost'),
             'PORT': os.getenv('DB_PORT', '5432'),
         }
